36d2b01c21fecdc240c2533662f783633b9f09df
tope-olajide <31278728+tope-olajide@users.noreply.github.com>
2024-12-03 12:55:08 +0100

    Temporarily disable linting during production builds

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: process.env.NODE_ENV === 'production', // Ignore TypeScript errors only in production
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig;
